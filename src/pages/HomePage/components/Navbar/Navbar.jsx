7567a5203c245ae3ad93269b53f2aa6c9ae1683c
Oghenetega Esedere <arcteggzz@gmail.com>
2025-02-13 13:32:47 +0100

    feat: first commits

---
+++ src/pages/HomePage/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className={"navbar"}>
+        <div className="nav-container"></div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
